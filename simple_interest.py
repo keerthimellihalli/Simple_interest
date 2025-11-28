fb045f20ce7282b49d72950f5342cfc32113674c
keerthimellihalli <keerthimellihalli@gmail.com>
2025-11-28 15:34:24 +0530

    Fix syntax and formatting issues in simple_interest.py

--- simple_interest.py
+++ simple_interest.py
@@ -1,13 +1,13 @@
 import sys
 
-if len(sys.argv)!=4:
-   print("usage:python simple_interest.py<principle><rate><time>")
-  sys.exit(1)
+if len(sys.argv) != 4:
+    print("usage: python simple_interest.py <principle> <rate> <time>")
+    sys.exit(1)
 
-P=float(sys.aryv[1])
-R=float(sys.argv[2])
-T=float(sys.argv[3])
+P = float(sys.argv[1])
+R = float(sys.argv[2])
+T = float(sys.argv[3])
 
-si = (P * R * T)/100
+si = (P * R * T) / 100
 
-print("simple Interest:",si)
+print("Simple Interest:", si)
